8894a487469b4c842f09a3492a0f5a8590dae1b5
yzsxh <1223924896@qq.com>
2023-05-06 18:38:31 +0800

    chore: add commitlint

--- .cz-config.js
+++ .cz-config.js
@@ -24,6 +24,7 @@ module.exports = {
     footer: '请输入要关闭的issue(可选):',
     confirmCommit: '确认使用以上信息提交？(y/n/e/h)'
   },
+  allowCustomScopes: true,
   // 跳过问题
   skipQuestions: ['body', 'footer'],
   // subject文字长度默认是72
